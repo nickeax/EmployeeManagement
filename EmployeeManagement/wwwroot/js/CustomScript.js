2fc8edb7c2f1cc25311447b7283e8d146cb8e4c1
Nick Fletcher <nickeax@gmail.com>
2020-11-29 14:31:23 +1100

    Enable delete confirmation buttons.

--- EmployeeManagement/wwwroot/js/CustomScript.js
+++ EmployeeManagement/wwwroot/js/CustomScript.js
@@ -1,1 +1,10 @@
-﻿
+﻿function confirmDelete(id, isDeleteClicked) {
+    console.log(id)
+    if (isDeleteClicked) {
+        $(`#deleteSpan_${id}`).hide()
+        $(`#confirmSpan_${id}`).show()
+    } else {
+        $(`#deleteSpan_${id}`).show()
+        $(`#confirmSpan_${id}`).hide()
+    }
+}
